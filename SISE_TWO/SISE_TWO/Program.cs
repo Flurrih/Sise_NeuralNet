22790e84baea03e9b0929b9b7456acf1ba4d91a2
Tomek <tom.gabryelewicz@gmail.com>
2017-06-11 22:35:34 +0200

    sise2

---
+++ SISE_TWO/SISE_TWO/Program.cs
@@ -0,0 +1,19 @@
+﻿using SISE_TWO;
+using System;
+namespace CodingBackProp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NeuralNet network = new NeuralNet();
+            network.GenerateRandom();
+            network.Run(10000);
+            Console.ReadKey();
+        }
+        
+    }
+
+
+
+}
